5be827882f058c8c35a4a01d9269802d87b46b84
Alam Rodriguez <alamrd2016@gmail.com>
2023-09-01 09:24:11 -0400

    packege cors added

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+
 const PORT = process.env.PORT || 3001;
+
+
 
 // Configura body-parser para manejar solicitudes JSON
 app.use(bodyParser.json());
